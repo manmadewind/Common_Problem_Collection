a6e614590e1c9d639612f4edbf4c8af359432c19
manmadewind <manmadewind@gmail.com>
2013-04-17 10:15:51 +0800

    Update Find_Right_Place.py

--- Find_Right_Place.py
+++ Find_Right_Place.py
@@ -4,10 +4,14 @@
 Q:一数组，要求求出所有这样的数a[i]——其左边的数都小于等于它，右边的数都大于等于它。
 只用一个额外数组和少量其他空间实现
 
+例子： 有数组 [4, 3, 6, 1, 2, 4, 9, 10]，则依题意元素9和10即为所求。
+
+
+我的解法:
 时间复杂度：O(n)
 算法思路如下：
-(1).先从左到右进行一次扫描，当找到比当前左侧的所有元素都大的元素时，记录下他。
-(2).再从右到左进行一次扫描，当找到比当前右侧的所有元素都小的元素时，记录下他。
+(1).先从左到右进行一次扫描，当找到比当前左侧的所有元素都大的元素时，记录下当前元素（在结果数组中做个标记）。
+(2).再从右到左进行一次扫描，当找到比当前右侧的所有元素都小的元素时，记录下当前元素（在结果数组中做个标记）。
 (3).取(1)(2)两个结果集中的交集即为所求。
 '''
 
@@ -19,9 +23,9 @@ def findRightPosition(array):
     for i in range(0, len(array)): # i in [ 0, len(array) )
         if array[i] >= leftMax: # 此元素为目前最大   
             leftMax = array[i]
-            results.append(1)
+            results.append(1) # 在结果数组中将该元素标记为1
         else:
-            results.append(0)
+            results.append(0) # 在结果数组中将其标记为0
 
     print results
     
@@ -29,9 +33,8 @@ def findRightPosition(array):
     rightMin = array[-1]
     for i in range(len(array) - 1, 0, -1):    
         if array[i] <= rightMin: # 此元素为目前最小
-            rightMin = array[i]
-            print i
-            results[i] += 1
+            rightMin = array[i] 
+            results[i] += 1 # 在结果数组中做标记
 
     # Step3. output results
     for i in range(0, len(array)):
